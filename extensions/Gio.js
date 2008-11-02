7fc1c8b77ab6cd751fde53539235bdc36c5963be
Robert Carr <racarr@src.gnome.org>
2008-11-02 10:26:11 +0000

    Add Gio.simple_write.
    svn path=/trunk/; revision=59

--- extensions/Gio.js
+++ extensions/Gio.js
@@ -2,7 +2,17 @@ prototype = Seed.prototype(Gio.FileInputStream);
 
 prototype.get_contents = function()
 {
-	stream = Gio.DataInputStream._new(this);
-	line = stream.read_until("", 0);
+	var stream = Gio.DataInputStream._new(this);
+	var line = stream.read_until("", 0);
 	return line;	
+}
+
+Gio.simple_write = function(file, name)
+{
+	var file = Gio.file_new_for_path(file);
+	var fstream = file.replace();
+	var dstream = Gio.DataOutputStream._new(fstream);
+	
+	dstream.put_string(name);
+	fstream.close();
 }
