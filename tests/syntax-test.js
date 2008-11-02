891073e1c5af616f083e8b3040c1ee40ace404cf
Robert Carr <racarr@src.gnome.org>
2008-11-02 23:01:47 +0000

    Add syntax error test.
    svn path=/trunk/; revision=63

---
+++ tests/syntax-test.js
@@ -0,0 +1,9 @@
+#!/usr/local/bin/seed
+// Returns: 0
+// STDIN:
+// STDOUT:
+// STDERR:\n\*\* \(seed:[0-9]+\): CRITICAL \*\*: SyntaxError\. Parse error in \.\/syntax-test\.js at line 6
+
+new = 3
+
+
