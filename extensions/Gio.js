6f7876e09483527b9e8a5cd1743fdab908a703eb
Robert Carr <racarr@src.gnome.org>
2008-11-02 07:24:24 +0000

    That's embarassing.
    svn path=/trunk/; revision=55

--- extensions/Gio.js
+++ extensions/Gio.js
@@ -2,7 +2,7 @@ prototype = Seed.prototype(Gio.FileInputStream);
 
 prototype.get_contents = function()
 {
-	stream = Gio.DataInputStream._new(input);
+	stream = Gio.DataInputStream._new(this);
 	line = stream.read_until("", 0);
 	return line;	
 }
