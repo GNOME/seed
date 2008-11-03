85a4f69f138f3523a0112e996c26afedbed8f19f
Robert Carr <racarr@src.gnome.org>
2008-11-03 10:38:31 +0000

    Add a test case for the previous commit.
    svn path=/trunk/; revision=86

---
+++ tests/property-glib-exception.js
@@ -0,0 +1,11 @@
+#!/usr/local/bin/seed
+// Returns: 0
+// STDIN:
+// STDOUT:
+// STDERR:\n\*\* \(seed:[0-9]+\): CRITICAL \*\*: PropertyError. value "3.000000" of type `gdouble' is invalid or out of range for property `opacity' of type `gdouble' in ./property-glib-exception.js at line 10
+
+Seed.import_namespace("Gtk");
+Gtk.init(null, null);
+
+window = new Gtk.Window();
+window.opacity = 3;
