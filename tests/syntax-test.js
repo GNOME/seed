91d0b338e6100bc1200b442b5f0c728fe9a31a41
Robert Carr <racarr@src.gnome.org>
2008-11-03 05:18:14 +0000

    Beginnings of "IDE".
    svn path=/trunk/; revision=67

--- tests/syntax-test.js
+++ tests/syntax-test.js
@@ -7,3 +7,12 @@
 new = 3
 
 
+#!/usr/local/bin/seed
+// Returns: 0
+// STDIN:
+// STDOUT:
+// STDERR:\n\*\* \(seed:[0-9]+\): CRITICAL \*\*: SyntaxError\. Parse error in \.\/syntax-test\.js at line 6
+
+new = 3
+
+
