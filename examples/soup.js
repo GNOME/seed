fbc56cdfcf4c906a2f1948367f3f65e974d901f2
Robert Carr <racarr@src.gnome.org>
2008-11-04 03:05:15 +0000

    Add simple soup example. Not very useful as soup isn't very bindable right now.
    svn path=/trunk/; revision=97

---
+++ examples/soup.js
@@ -0,0 +1,11 @@
+#!/usr/local/bin/seed
+Seed.import_namespace("Soup");
+
+session = new Soup.SessionSync();
+uri = Soup.URI._new("http://www.google.com");
+request = new Soup.Message({method:"GET",
+			    uri:uri});
+status = session.send_message(request);
+Seed.print(status);
+			    
+
