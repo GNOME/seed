1e445d4fd396ddb35b640441ccf247a00082af13
Robert Carr <racarr@src.gnome.org>
2008-11-05 11:10:57 +0000

    Add pango fontset example, queries fontsets on system and prints. Needs to be worked in to pango example (Tim!).
    svn path=/trunk/; revision=124

---
+++ examples/pango-fontset.js
@@ -0,0 +1,19 @@
+#!/usr/local/bin/seed
+
+Seed.import_namespace("Pango");
+Seed.import_namespace("PangoFT2");
+
+context = new Pango.Context();
+description = Pango.FontDescription._new();
+description.set_family("");
+
+fontmap = new PangoFT2.FontMap();
+fontset = fontmap.load_fontset(context, 
+			       description, 
+			       Pango.language_get_default());
+fontset.foreach(
+		function(fontset, font)
+		{
+		    description = font.describe();
+		    Seed.print(description.get_family());
+		});
