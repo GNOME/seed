515c799889f387118b76ba1ae9843a86d598bfc6
Robert Carr <racarr@src.gnome.org>
2008-11-07 07:34:11 +0000

    Add test of JSON.stringify and JSON.parse.
    svn path=/trunk/; revision=159

---
+++ tests/json.js
@@ -0,0 +1,14 @@
+#!/usr/local/bin/seed
+// Returns: 0
+// STDIN:
+// STDOUT:
+// STDERR:
+a = {test: 3,
+     hello: "Goodbye",
+     more: {a: "1", b: 2, c:"d"}};
+json = JSON.stringify(a);
+object = JSON.parse(json);
+json2 = JSON.stringify(object);
+if (json != json2)
+    Seed.print("Failure");
+       
