aaae536810591003f1f4512842a36a50a2d6b3d2
Robert Carr <racarr@src.gnome.org>
2008-11-08 05:58:50 +0000

    Remove broken tag.
    svn path=/tags/seed-0.1; revision=206

--- tests/argv.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env seed
-// Returns: 0
-// STDIN:
-// STDOUT:seed
-// STDERR:
-Seed.print(Seed.argv[0]);
--- tests/readline.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env seed
-// Returns: 0
-// STDIN:2+2
-// STDOUT:4.000000
-// STDERR:
-
-Seed.print(eval(Seed.readline("")));
