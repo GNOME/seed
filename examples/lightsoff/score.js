a5207c8265ac92bd5c39bd0784300cedc1846e07
Tim Horton <hortont@src.gnome.org>
2008-11-11 16:23:06 +0000

    Update score display.
    svn path=/trunk/; revision=251

--- examples/lightsoff/score.js
+++ examples/lightsoff/score.js
@@ -9,13 +9,34 @@ ScoreType = {
     name: "Score",
     class_init: function(klass, prototype)
     {
-
+		prototype.set_value = function (val)
+		{
+			var val_s = Seed.sprintf("% 5d",val);
+		
+			for(var i = 0; i < val_s.length; i++)
+			{
+				var texture = this.num_textures[val_s[i]];
+		
+				if(val_s[i] == ' ')
+					texture = null;
+			
+				var num = new Clutter.CloneTexture({parent_texture:texture});
+				num.set_position(56*i-10,3);
+				num.set_size(97,97);
+				this.add_actor(num);
+				num.show();
+			}
+			
+			this.bkg_top.raise_top();
+		}
     },
     instance_init: function(klass)
     {
+    	this.bkg_top = Clutter.Texture.new_from_file("./bkg_top.png");
 		var bkg = Clutter.Texture.new_from_file("./bkg.png");
 		var off_svg = Clutter.Texture.new_from_file("./off.svg");
 		
+		this.bkg_top.filter_quality = Clutter.TextureQuality.high;
 		bkg.filter_quality = Clutter.TextureQuality.high;
 		off_svg.filter_quality = Clutter.TextureQuality.high;
 		
@@ -24,26 +45,36 @@ ScoreType = {
 		for(var i = 0; i < 5; i++)
 		{
 			var off_i = new Clutter.CloneTexture({parent_texture:off_svg});
-			off_i.set_position(50*i-10,0);
-			off_i.set_size(80,80);
+			off_i.set_position(56*i-10,3);
+			off_i.set_size(97,97);
 			this.add_actor(off_i);
 		}
 		
-		var seven = Clutter.Texture.new_from_file("./7.svg");
-		seven.filter_quality = Clutter.TextureQuality.high;
-		seven.set_position(-10,0);
-		seven.set_size(80,80);
-		this.add_actor(seven);
+		this.num_textures = new Array();
+		
+		for(var i = 0; i <= 9; i++)
+		{
+			this.num_textures[i] = Clutter.Texture.new_from_file("./"+i+".svg");
+			this.num_textures[i].filter_quality = Clutter.TextureQuality.high;
+		}
+		
+		this.bkg_top.opacity = 150;
+		this.bkg_top.set_position(1,1);
+		this.add_actor(this.bkg_top);
+		
+		this.set_value(5123);
     }};
 
 Score = new GType(ScoreType);
 
-var stage = new Clutter.Stage();
-stage.signal.hide.connect(Clutter.main_quit);
+var black = Clutter.Color._new();
+Clutter.color_parse("Black", black);
+
+var stage = new Clutter.Stage({color: black});stage.signal.hide.connect(Clutter.main_quit);
 stage.set_size(400,400);
 
 board = new Score();
-
+board.set_position(20,20);
 stage.add_actor(board);
 stage.show_all();
 
