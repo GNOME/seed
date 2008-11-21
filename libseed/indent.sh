7db59913b9f6b3d86b0a7295681c26bdd80cb479
Tim Horton <hortont@src.gnome.org>
2008-11-21 18:23:39 +0000

    Fix broken indent script (didn't change tab size to 4 even though indent was 4, so lots of lines still used spaces!)
    svn path=/trunk/; revision=280

--- libseed/indent.sh
+++ libseed/indent.sh
@@ -1,5 +1,5 @@
 indent     -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -nce -ci4 \
             -cli0 -d0 -di1 -nfc1 -i4 -ip0 -l80 -lp -npcs -nprs -npsl -sai \
-            -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1 -bl -bli0 *.c *.h
+            -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts4 -il1 -bl -bli0 *.c *.h
 
 
