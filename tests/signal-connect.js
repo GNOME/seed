0f14d6b0335f113b0de6ab02647fca52d4e39e0a
Tim Horton <hortont@src.gnome.org>
2008-11-23 04:59:43 +0000

    Add object.connect test.
    svn path=/trunk/; revision=312

---
+++ tests/signal-connect.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env seed
+// Returns: 0
+// STDIN:
+// STDOUT:\[object GtkButton\]\n1\.000000\n0\.500000
+// STDERR:
+
+Seed.import_namespace("Gtk");
+Gtk.init(null, null);
+
+function handle_opacity_change(obj, gobject, user_data)
+{
+	Seed.print(user_data);
+	Seed.print(user_data === button);
+	Seed.print(obj.opacity);
+}
+
+win = new Gtk.Window();
+button = new Gtk.Button();
+win.connect("notify::opacity", handle_opacity_change, button);
+win.opacity = 0.5;
+
