424740ce04a6dcfee7ac71f503cdc636b4104a91
Robert Carr <racarr@src.gnome.org>
2008-12-11 00:49:32 +0000

    Cleanup accelgroup example.
    svn path=/trunk/; revision=429

--- examples/accelgroup.js
+++ examples/accelgroup.js
@@ -7,9 +7,12 @@ Gtk.init(null, null);
 w = new Gtk.Window();
 group = new Gtk.AccelGroup();
 group.connect(Gdk.keyval_from_name("q"), 
-	      0, 
-	      0, 
-	      function(){Gtk.main_quit()});
+			  0, 
+			  0, 
+			  function()
+			  {
+				  Gtk.main_quit()
+			  });
 w.add_accel_group(group);
 
 label = new Gtk.Label({label: "Press Q to quit"});
--- examples/notify-test.js
+++ examples/notify-test.js
@@ -9,7 +9,7 @@ function file_changed(monitor, child, other, event)
 {
     var notification = 
 	new Notify.Notification({summary: "File Notification",
-                                 body : "It's not clear what notification system this file is providing an example of." });
+                             body : "It's not clear what notification system this file is providing an example of." });
     notification.set_timeout(5000);
     notification.show();
 }
