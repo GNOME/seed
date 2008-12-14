cda177cf23d9151acc67ac2c026db83c5a11db2b
Robert Carr <racarr@src.gnome.org>
2008-12-14 07:56:47 +0000

    Add struct enumeration tests, including a recursive enumeration of GdkEvent.
    svn path=/trunk/; revision=457

---
+++ tests/struct-enumerate.js
@@ -0,0 +1,12 @@
+#!/usr/local/bin/seed
+// Returns: 0
+// STDIN:
+// STDOUT:x\ny\nwidth\nheight\nintersect\nunion
+// STDERR:
+Seed.import_namespace("Gdk");
+
+r = new Gdk.Rectangle();
+for (prop in r)
+{
+	Seed.print(prop);
+}
