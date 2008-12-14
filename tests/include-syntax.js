7453703df497551f2a66e11c4d5b304e559ac30a
Robert Carr <racarr@src.gnome.org>
2008-12-14 06:29:33 +0000

    Add test of Seed.include syntax exception.
    svn path=/trunk/; revision=442

---
+++ tests/include-syntax.js
@@ -0,0 +1,6 @@
+#!/usr/local/bin/seed
+// Returns: 0
+// STDIN:
+// STDOUT:
+// STDERR:\n\*\* \(seed:[0-9]+\): CRITICAL \*\*: SyntaxError\. Parse error in syntax-test\.js at line 6
+Seed.include("syntax-test.js");
