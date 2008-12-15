dcaf38a4f986f31bbe3126a14716c3be7368ce85
Robert Carr <racarr@src.gnome.org>
2008-12-15 08:53:22 +0000

    Add threaded repl example. Still a little screwy, but can do things like manipulate objects, connect signals, etc, without worrying about any threading issues from JS.
    svn path=/trunk/; revision=478

---
+++ examples/threaded-repl.js
@@ -0,0 +1,10 @@
+#!/usr/local/bin/seed
+Seed.import_namespace("Gtk");
+Seed.import_namespace("GLib");
+Gtk.init(null, null);
+var w = new Gtk.Window();
+
+//Can interact with window at threaded repl.
+Seed.thread_repl();
+
+Gtk.main();
