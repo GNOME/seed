2d87d31439b9e6b11f90904396937773661aac0c
Robert Carr <racarr@src.gnome.org>
2008-12-16 00:24:01 +0000

    Add Gnio example.
    svn path=/trunk/; revision=480

---
+++ examples/Gnio.js
@@ -0,0 +1,24 @@
+#!/usr/local/bin/seed
+Seed.import_namespace("Gio");
+Seed.import_namespace("Gnio");
+
+r = new Gnio.Resolver();
+
+address = r.lookup_name("www.google.com");
+saddr = new Gnio.InetSocketAddress({address: address, port: 80});
+client = new Gnio.SocketConnection({address: saddr});
+
+client.connect(null);
+
+message = "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n";
+
+os = client.get_output_stream();
+ds = Gio.DataOutputStream._new(os);
+ds.put_string(message);
+
+os.flush();
+
+input = Gio.DataInputStream._new(client.get_input_stream());
+
+out = input.read_until("", 0);
+Seed.print(out);
