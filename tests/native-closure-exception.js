b922ee6644377842cfaa17a69e3a6fd47ad5c8c6
Robert Carr <racarr@src.gnome.org>
2008-12-20 03:04:17 +0000

    Add a test of native closure exceptions.
    svn path=/trunk/; revision=499

---
+++ tests/native-closure-exception.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env seed
+// Returns: 0
+// STDIN:
+// STDOUT:
+// STDERR:\n\*\* \(seed:[0-9]+\): WARNING \*\*: Exception in closure marshal\. Line 11 in \.\/native-closure-exception\.js: ReferenceError Can't find variable: a
+Seed.import_namespace("Gtk")
+Gtk.init(null, null);
+
+w = new Gtk.Window();
+vbox = new Gtk.VBox();
+
+closure = function(widget){a.a.a};
+w.add(vbox);
+
+vbox.pack_start(new Gtk.Label());
+
+vbox.foreach(closure);
