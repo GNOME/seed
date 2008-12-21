8c41fa486f28b84acfe79a2fbdd07cdf3a89e6e8
Robert Carr <racarr@src.gnome.org>
2008-12-21 06:09:04 +0000

    Relicense under LGPL3. This change has been approved by all contributors.
    svn path=/trunk/; revision=530

--- libseed/seed-api.c
+++ libseed/seed-api.c
@@ -1,3 +1,20 @@
+/*    This file is part of Seed.
+*     Seed is free software: you can redistribute it and/or modify 
+*     it under the terms of the GNU Lesser General Public License as
+*     published by 
+*     the Free Software Foundation, either version 3 of the License, or 
+*     (at your option) any later version. 
+*     Seed is distributed in the hope that it will be useful, 
+*     but WITHOUT ANY WARRANTY; without even the implied warranty of 
+*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+*     GNU Lesser General Public License for more details. 
+*     You should have received a copy of the GNU Lesser General Public License 
+*     along with Seed.  If not, see <http://www.gnu.org/licenses/>. 
+*
+*     Copyright (C) Robert Carr 2008 <carrr@rpi.edu>
+*/
+
+
 #include "seed-private.h"
 
 void seed_value_protect(JSContextRef ctx, JSValueRef value)
--- libseed/seed-structs.c
+++ libseed/seed-structs.c
@@ -1,23 +1,22 @@
 /*
  * -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- 
  */
-/*
- * seed-structs.h
- * Copyright (C) Robert Carr 2008 <carrr@rpi.edu>
- *
- * libseed is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Gener1al Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * libseed is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*    This file is part of Seed.
+*     Seed is free software: you can redistribute it and/or modify 
+*     it under the terms of the GNU Lesser General Public License as
+*     published by 
+*     the Free Software Foundation, either version 3 of the License, or 
+*     (at your option) any later version. 
+*     Seed is distributed in the hope that it will be useful, 
+*     but WITHOUT ANY WARRANTY; without even the implied warranty of 
+*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+*     GNU Lesser General Public License for more details. 
+*     You should have received a copy of the GNU Lesser General Public License 
+*     along with Seed.  If not, see <http://www.gnu.org/licenses/>. 
+*
+*     Copyright (C) Robert Carr 2008 <carrr@rpi.edu>
+*/
+
 
 #include "seed-private.h"
 #include <string.h>
