7a282eadc71315a3e8e6a3744ee1c202bfffd497
Tim Horton <hortont@src.gnome.org>
2008-12-28 04:03:42 +0000

    Cleaning up tutorial browser.
    svn path=/trunk/; revision=567

--- doc/tutorial-standalone/2.js
+++ doc/tutorial-standalone/2.js
@@ -11,6 +11,7 @@ TabbedBrowser = new GType({
     name: "TabbedBrowser",
     instance_init: function (klass)
     {
+        // Public
         this.close_tab = function (tab)
         {
             browser.remove_page(browser.page_num(tab));
@@ -48,6 +49,7 @@ TabbedBrowser = new GType({
             return this.get_nth_page(this.page);
         };
 
+        // Implementation
         this.new_tab(home_page);
     }
 });
@@ -57,22 +59,13 @@ BrowserTab = new GType({
     name: "BrowserTab",
     instance_init: function (klass)
     {
+        // Private
         var toolbar = new BrowserToolbar();
         var web_view = new BrowserView();
         var scroll_view = new Gtk.ScrolledWindow();
         var tab_label;
         
-        web_view.set_tab(this);
-        
-        scroll_view.smooth_scroll = true;
-        scroll_view.add(web_view);
-        scroll_view.set_policy(Gtk.PolicyType.Automatic,
-                               Gtk.PolicyType.Automatic);
-
-        this.pack_start(toolbar);
-        this.pack_start(scroll_view, true, true);
-        this.show_all();
-
+        // Public
         this.get_toolbar = function ()
         {
             return toolbar;
@@ -92,6 +85,18 @@ BrowserTab = new GType({
         {
             return tab_label;
         };
+        
+        // Implementation
+        web_view.set_tab(this);
+        
+        scroll_view.smooth_scroll = true;
+        scroll_view.add(web_view);
+        scroll_view.set_policy(Gtk.PolicyType.Automatic,
+                               Gtk.PolicyType.Automatic);
+
+        this.pack_start(toolbar);
+        this.pack_start(scroll_view, true, true);
+        this.show_all();
     }
 });
 
@@ -100,25 +105,8 @@ BrowserView = new GType({
     name: "BrowserView",
     instance_init: function (klass)
     {
+        // Private
         var tab;
-        
-        this.browse = function (url)
-        {
-            if(url.search("://") < 0)
-                url = "http://" + url;
-
-            this.open(url);
-        };
-        
-        this.set_tab = function (new_tab)
-        {
-            tab = new_tab;
-        }
-        
-        this.get_tab = function ()
-        {
-            return tab;
-        };
         
         var update_title = function (web_view, web_frame, title)
         {
@@ -137,6 +125,26 @@ BrowserView = new GType({
             toolbar.set_can_go_forward(web_view.can_go_forward());
         };
         
+        // Public
+        this.browse = function (url)
+        {
+            if(url.search("://") < 0)
+                url = "http://" + url;
+
+            this.open(url);
+        };
+        
+        this.set_tab = function (new_tab)
+        {
+            tab = new_tab;
+        }
+        
+        this.get_tab = function ()
+        {
+            return tab;
+        };
+        
+        // Implementation
         this.set_scroll_adjustments(null, null);
         this.signal.title_changed.connect(update_title);
         this.signal.load_committed.connect(update_url);
@@ -148,6 +156,7 @@ BrowserToolbar = new GType({
     name: "BrowserToolbar",
     instance_init: function (klass)
     {
+        // Private
         var url_bar = new Gtk.Entry();
 
         var back_button = new Gtk.ToolButton({stock_id:"gtk-go-back"});
@@ -174,11 +183,7 @@ BrowserToolbar = new GType({
             browser.current_tab().get_web_view().browse(url.text);
         };
 
-        back_button.signal.clicked.connect(back);
-        forward_button.signal.clicked.connect(forward);
-        refresh_button.signal.clicked.connect(refresh);
-        url_bar.signal.activate.connect(browse);
-        
+        // Public
         this.set_url = function (url)
         {
             url_bar.text = url;
@@ -193,6 +198,12 @@ BrowserToolbar = new GType({
         {
             forward_button.sensitive = can_go_forward;
         };
+        
+        // Implementation
+        back_button.signal.clicked.connect(back);
+        forward_button.signal.clicked.connect(forward);
+        refresh_button.signal.clicked.connect(refresh);
+        url_bar.signal.activate.connect(browse);
 
         this.pack_start(back_button);
         this.pack_start(forward_button);
