75c2dfa25e370a09cebb837758ffb99f388cebaa
Tim Horton <hortont@src.gnome.org>
2009-01-08 20:56:50 +0000

    ROBB! It's /usr/bin/env seed...
    svn path=/trunk/; revision=686

--- examples/json-reddit.js
+++ examples/json-reddit.js
@@ -1,5 +1,7 @@
-#!/usr/local/bin/seed
+#!/usr/bin/env seed
+
 Seed.import_namespace("Gio");
+
 var redditurl = Gio.file_new_for_uri("http://reddit.com/.json");
 var reddit = JSON.parse(redditurl.read().get_contents());
 
