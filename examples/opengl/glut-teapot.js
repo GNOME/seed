08804c861e01b38737faee7eabf706e168d26edb
Robert Carr <racarr@src.gnome.org>
2009-01-19 21:08:57 +0000

    Add OpenGL examples.
    svn path=/trunk/; revision=773

---
+++ examples/opengl/glut-teapot.js
@@ -0,0 +1,53 @@
+#!/usr/local/bin/seed
+Seed.import_namespace("GL");
+Seed.import_namespace("GLUT");
+
+
+var mat_specular = [1.0,1.0,1.0,1.0];
+var mat_shininess = [50.0];
+var light_position = [0.0,0.0,-1.0,0.0];
+
+GLUT.Init(null, null);
+
+GLUT.InitDisplayMode(GLUT.DEPTH | GLUT.RGBA | GLUT.DOUBLE);
+
+
+GLUT.InitWindowSize(640, 480);
+
+GLUT.CreateWindow("Oh dear god");
+
+
+GLUT.DisplayFunc(function()
+					 {
+						 with (GL)
+						 {
+							 Clear(DEPTH_BUFFER_BIT | COLOR_BUFFER_BIT);
+							 PushMatrix();
+							  Begin(TRIANGLES);
+							   Color3f(1,0,0);
+							   Vertex3f(-1,-1,0);
+							   Color3f(0,1,0);
+							   Vertex3f(1,-1,0);
+							   Color3f(0,0,1);
+							   Vertex3f(0,1,0);
+							  End();		
+							 PopMatrix();
+						 }
+						 GLUT.SolidTeapot(0.5);
+						 GLUT.SwapBuffers();
+					 });
+
+
+GL.ClearColor(0.0,0.0,0.0,0.0);
+GL.ShadeModel(GL.SMOOTH);
+
+//GL.glMaterialfv(GL.FRONT, GL.SPECULAR, mat_specular);
+//GL.glMaterialfv(GL.FRONT, GL.SHININESS, mat_shininess);
+GL.Lightfv(GL.LIGHT0, GL.POSITION, light_position);
+
+GL.Enable(GL.LIGHTING);
+GL.Enable(GL.LIGHT0);
+
+
+GLUT.MainLoop();
+
---
+++ examples/opengl/triangle-actor.js
@@ -0,0 +1,36 @@
+#!/usr/local/bin/seed
+Seed.import_namespace("Clutter");
+Seed.import_namespace("GL");
+
+Clutter.init(null, null);
+
+var s = new Clutter.Stage();
+
+var b = new Clutter.Rectangle();
+
+b.width = b.x = s.width/2;
+
+b.y = 250;
+
+s.add_actor(b);
+
+b.signal["paint"].connect(function(actor)
+						  {
+							  with(GL)
+							  {
+								  PushMatrix();
+								   Scalef(200,200,1);
+								   Begin(TRIANGLES);
+								    Color3f(1,0,0);
+  								    Vertex3f(-1,-1,0);
+								    Color3f(0,1,0);
+								    Vertex3f(1,-1,0);
+								    Color3f(0,0,1);
+								    Vertex3f(0,1,0);
+								   End();		
+								  PopMatrix();
+							  }
+						  });
+
+s.show();
+Clutter.main();
