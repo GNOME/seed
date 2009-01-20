c8fc42141a1db727869242cca15547779b9563ad
Robert Carr <racarr@src.gnome.org>
2009-01-20 18:33:57 +0000

    Add async quine example
    svn path=/trunk/; revision=775

---
+++ examples/async-quine.js
@@ -0,0 +1,19 @@
+#!/usr/local/bin/seed
+Seed.import_namespace("GLib");
+Seed.import_namespace("Gio");
+
+
+var file = Gio.file_new_for_path("./async-quine.js");
+
+file.read_async(0, null, function(source, result)
+				{
+					var stream = source.read_finish(result);
+					var dstream = new Gio.DataInputStream.c_new(stream);
+					var data = dstream.read_until("", 0);
+					
+					Seed.print(data);
+					Seed.quit();
+				});
+
+var loop = GLib.main_loop_new();
+GLib.main_loop_run(loop);
