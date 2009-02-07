ec254cb7f301203c85220d6377a9659360cfd65c
Tim Horton <hortont@src.gnome.org>
2009-02-07 08:37:45 +0000

    Consistent headers across the C files.
    svn path=/trunk/; revision=849

--- libseed/seed-exceptions.c
+++ libseed/seed-exceptions.c
@@ -1,22 +1,22 @@
 /*
  * -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- 
  */
+
 /*
- * seed-exceptions.c
- * Copyright (C) Robert Carr 2008 <carrr@rpi.edu>
+ *	This file is part of Seed, the GObject Introspection<->Javascript bindings.
  *
- * libseed is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *		Seed is free software: you can redistribute it and/or modify 
+ *		it under the terms of the GNU Lesser General Public License as
+ *		published by the Free Software Foundation, either version 3 of
+ *		the License, or (at your option) any later version. 
+ *		Seed is distributed in the hope that it will be useful, 
+ *		but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ *		GNU Lesser General Public License for more details. 
+ *		You should have received a copy of the GNU Lesser General Public License 
+ *		along with Seed.  If not, see <http://www.gnu.org/licenses/>. 
  *
- * libseed is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *		Copyright (C) Robert Carr 2008 <carrr@rpi.edu>
  */
 
 #include "seed-private.h"
--- libseed/seed-types.c
+++ libseed/seed-types.c
@@ -2,21 +2,21 @@
  * -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- 
  */
 
-/*    This file is part of Seed.
- *     Seed is free software: you can redistribute it and/or modify 
- *     it under the terms of the GNU Lesser General Public License as
- *     published by 
- *     the Free Software Foundation, either version 3 of the License, or 
- *     (at your option) any later version. 
- *     Seed is distributed in the hope that it will be useful, 
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- *     GNU Lesser General Public License for more details. 
- *     You should have received a copy of the GNU Lesser General Public License 
- *     along with Seed.  If not, see <http://www.gnu.org/licenses/>. 
+/*
+ *	This file is part of Seed, the GObject Introspection<->Javascript bindings.
  *
- *     Copyright (C) Robert Carr 2008 <carrr@rpi.edu>
- *     Copyright (C) Tim Horton 2008 <hortont424@gmail.com>
+ *		Seed is free software: you can redistribute it and/or modify 
+ *		it under the terms of the GNU Lesser General Public License as
+ *		published by the Free Software Foundation, either version 3 of
+ *		the License, or (at your option) any later version. 
+ *		Seed is distributed in the hope that it will be useful, 
+ *		but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ *		GNU Lesser General Public License for more details. 
+ *		You should have received a copy of the GNU Lesser General Public License 
+ *		along with Seed.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ *		Copyright (C) Robert Carr 2008 <carrr@rpi.edu>
  */
 
 #include "seed-private.h"
