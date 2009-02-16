3775824bb6d2ee01ff1c0b76f4b88ffe5745ec93
Tim Horton <hortont@Kaylee.(none)>
2009-02-16 16:15:09 -0500

    Didn't mean to add animate_with_timeline in the last push.

--- extensions/Clutter.js
+++ extensions/Clutter.js
@@ -10,14 +10,3 @@ Clutter.Actor.prototype.animate = function(mode, duration, json)
 
 }
 
-Clutter.Actor.prototype.animate_with_timeline = function(mode, timeline, json)
-{
-	var properties = new Array();
-	var endvalues = new Array();
-	for (var prop in json)	{
-		properties.push(prop);
-		endvalues.push(json[prop]);
-	}
-	return this.animatev_with_timeline(mode, timeline, properties, endvalues);
-
-}
