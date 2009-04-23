2ebc9e50f8549062f9a1895fa26997463f6078af
Robert Carr <racarr@svn.gnome.org>
2009-04-23 17:40:35 -0400

    Rename readline_bind test to readline-bind, to keep convention

---
+++ tests/javascript/readline-bind.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env seed
+// Returns: 0
+// STDIN:a 2+2
+// STDOUT:Got here!\n4.000000
+// STDERR:
+
+readline = imports.readline;
+readline.bind("a", function () { Seed.print("Got here!"); });
+Seed.print(eval(readline.readline("")));
