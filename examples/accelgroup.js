ee922caef45a1055f187253bb00c1613ec4b8d7b
Robert Carr <racarr@svn.gnome.org>
2009-04-28 15:31:59 -0400

    Update accelgroup example to use more...modern seed

--- examples/accelgroup.js
+++ examples/accelgroup.js
@@ -3,27 +3,19 @@
 Gtk = imports.gi.Gtk;
 Gdk = imports.gi.Gdk;
 
-with (Gtk)
-{
-    init(null, null);
+Gtk.init(null, null);
 
-    var quit = function ()
-    {
-	main_quit();
-    };
+var w = new Gtk.Window();
+w.signal.hide.connect(Gtk.main_quit);
 
-    var w = new Window();
-    w.signal.hide.connect(quit);
+var group = new Gtk.AccelGroup();
+group.connect(Gdk.keyval_from_name("q"), 0, 0, Gtk.main_quit);
+w.add_accel_group(group);
 
-    var group = new AccelGroup();
-    group.connect(Gdk.keyval_from_name("q"), 0, 0, quit);
-    w.add_accel_group(group);
+var label = new Gtk.Label({label: "Press Q to quit"});
+w.add(label);
 
-    var label = new Label({label: "Press Q to quit"});
-    w.add(label);
+w.resize(300, 300);
+w.show_all();
 
-    w.resize(300, 300);
-    w.show_all();
-
-    main();
-}
+Gtk.main();
