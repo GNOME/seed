a1d0d2b62e1da482773bdafc316e1bd251b27b51
Robert Carr <racarr@svn.gnome.org>
2009-04-28 15:58:46 -0400

    Update gdkpixbufloader example style

--- examples/gdkpixbufloader.js
+++ examples/gdkpixbufloader.js
@@ -8,27 +8,23 @@ Gtk = imports.gi.Gtk;
 var blue_marble =
     Gio.file_new_for_uri("http://veimages.gsfc.nasa.gov/7100/world.topo.bathy.200401.3x5400x2700.jpg");
 
-blue_marble.read_async(0, null, function(source, result)
-		       {
-			   var loader = new GdkPixbuf.PixbufLoader();
-			   var stream = source.read_finish(result);
-			   var dstream = new Gio.DataInputStream.c_new(stream);
+blue_marble.read_async(0, null, function(source, result){
+    var loader = new GdkPixbuf.PixbufLoader();
+    var stream = source.read_finish(result);
+    var dstream = new Gio.DataInputStream.c_new(stream);
 
-			   try
-			   {
-			       while (1)
-			       {
-				   loader.write([dstream.read_byte()], 1);
-			       }
-			   }
-			   catch (e)
-			   {
-			   }
+    try{
+	while (1){
+	    loader.write([dstream.read_byte()], 1);
+	}
+    }
+    catch (e){
+    }
 
-			   var pixbuf = loader.get_pixbuf();
-			   pixbuf.savev("bluemarble","jpeg");
-			   Seed.quit();
-		       });
+    var pixbuf = loader.get_pixbuf();
+    pixbuf.savev("bluemarble","jpeg");
+    Seed.quit();
+});
 
 var loop = GLib.main_loop_new();
 GLib.main_loop_run(loop);
