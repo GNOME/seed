33236526dca506b8e75d838682b59df3b8a7a76a
Robert Carr <racarr@svn.gnome.org>
2009-04-28 16:07:53 -0400

    Update vte-test example style, and remove old Gtk.main_quit hack

--- examples/soup.js
+++ examples/soup.js
@@ -5,7 +5,7 @@ Soup = imports.gi.Soup;
 var session = new Soup.SessionSync();
 
 // Soup.URI is a struct.
-var uri = Soup.URI.c_new("http://www.google.com");
+var uri = new Soup.URI.c_new("http://www.google.com");
 var request = new Soup.Message({method:"GET", uri:uri});
 
 var status = session.send_message(request);
--- examples/vte-test.js
+++ examples/vte-test.js
@@ -3,33 +3,29 @@
 Gtk = imports.gi.Gtk;
 Vte = imports.gi.Vte;
 
-with (Gtk)
-{
-    init(null, null);
-    
-    var window = new Window();
-    window.signal.hide.connect(function () { Gtk.main_quit(); } );
+Gtk.init(null, null);
 
-    var scroll = new ScrolledWindow();
+var window = new Gtk.Window();
+window.signal.hide.connect(Gtk.main_quit);
 
-    var vte = new Vte.Terminal();
-    vte.fork_command("/bin/bash");
-    vte.signal.child_exited.connect(function(){main_quit();});
-    vte.signal.window_title_changed.connect
-    (function(terminal)
-     {
-	 window.set_title(terminal.get_window_title());
-     });
+var scroll = new Gtk.ScrolledWindow();
+
+var vte = new Vte.Terminal();
+vte.fork_command("/bin/bash");
+vte.signal.child_exited.connect(Gtk.main_quit);
+vte.signal.window_title_changed.connect(
+    function(terminal){
+	window.set_title(terminal.get_window_title());
+    });
 
 
-    scroll.add(vte);
-    scroll.set_policy(PolicyType.AUTOMATIC,
-		      PolicyType.AUTOMATIC);
+scroll.add(vte);
+scroll.set_policy(Gtk.PolicyType.AUTOMATIC,
+		  Gtk.PolicyType.AUTOMATIC);
 
-    window.add(scroll);
-    window.resize(600,400);
-    window.show_all();
+window.add(scroll);
+window.resize(600,400);
+window.show_all();
 
-    main();
+Gtk.main();
 
-}
