183d310c2ca8f784ec93ef3d3d700c12aebe3a0f
Robert Carr <racarr@svn.gnome.org>
2009-04-28 16:02:41 -0400

    Update style in json-reddit example

--- examples/json-reddit.js
+++ examples/json-reddit.js
@@ -7,14 +7,13 @@ var reddit = JSON.parse(redditurl.read().get_contents());
 
 stories = reddit.data.children;
 
-for (i in stories)
-{
-	var story = stories[i].data;
-	Seed.print("======");
-	Seed.print(story.ups + "/" +
-			   story.downs + " " +
-			   story.title + " (" +
-			   story.domain +")" );
-	Seed.print(story.author);
+for (i in stories){
+    var story = stories[i].data;
+    Seed.print("======");
+    Seed.print(story.ups + "/" +
+	       story.downs + " " +
+	       story.title + " (" +
+	       story.domain +")" );
+    Seed.print(story.author);
 }
 
