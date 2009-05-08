9f925b756e4407a0f2402b9848b628b8fe935d8c
Robert Carr <racarr@svn.gnome.org>
2009-05-07 21:13:34 -0400

    Pango fontset example isn't really broken, but need to use C allocator for Pango.FontDescription.

---
+++ examples/pango-fontset.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env seed
+
+Pango = imports.gi.Pango;
+PangoFT2 = imports.gi.PangoFT2;
+
+context = new Pango.Context();
+description = new Pango.FontDescription.c_new();
+description.set_family("");
+fontmap = new PangoFT2.FontMap();
+fontset = fontmap.load_fontset(context, 
+			       description, 
+			       Pango.language_get_default());
+fontset.foreach(function(fontset, font){
+    description = font.describe();
+    Seed.print(description.to_string());
+});
