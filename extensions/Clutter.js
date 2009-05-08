fee20fc2c75f67822582d5e4f2e316608e4c0db0
Tim Horton <hortont@svn.gnome.org>
2009-05-07 22:39:03 -0400

    Clutter added animate_with_timelinev, so we should have an extension for that as well.

--- extensions/Clutter.js
+++ extensions/Clutter.js
@@ -13,3 +13,15 @@ Clutter.Actor.prototype.animate = function(mode, duration, json)
 
 }
 
+Clutter.Actor.prototype.animate_with_timeline = function(mode, timeline, json)
+{
+	var properties = new Array();
+	var endvalues = new Array();
+	for (var prop in json)	{
+		properties.push(prop);
+		endvalues.push(json[prop]);
+	}
+	return this.animate_with_timelinev(mode, timeline, properties.length, 
+			     properties, endvalues);
+
+}
