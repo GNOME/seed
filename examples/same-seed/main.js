d2829e140beb0a29c2e5231369e31f6b05aa691a
Tim Horton <hortont@hortont.com>
2009-05-10 03:13:52 -0400

    Same Seed: clutter-gtk seems to be broken; revert from GtkBuilder UI to pure Clutter for now

--- examples/same-seed/main.js
+++ examples/same-seed/main.js
@@ -11,11 +11,9 @@ var max_colors = 3;
 var fly_score = true;
 
 imports.gi.versions.Clutter = "0.9";
-imports.gi.versions.GtkClutter = "0.9";
 
 Gtk = imports.gi.Gtk;
 Clutter = imports.gi.Clutter;
-GtkClutter = imports.gi.GtkClutter;
 GdkPixbuf = imports.gi.GdkPixbuf;
 GConf = imports.gi.GConf;
 GLib = imports.gi.GLib;
@@ -23,7 +21,6 @@ Pango = imports.gi.Pango;
 GObject = imports.gi.GObject;
 
 Gtk.init(null, null);
-GtkClutter.init(null, null);
 Clutter.init(null, null);
 GConf.init(null, null);
 
@@ -34,28 +31,18 @@ Seed.include("score.js");
 var score = 0;
 var timelines = [];
 
-var gb = new Gtk.Builder();
-gb.add_from_file("same-seed.ui");
+var stage = new Clutter.Stage();
 
-var window = gb.get_object("main_window");
-var clutter = gb.get_object("clutter");
-var stage = clutter.get_stage();
-
-window.signal.hide.connect(Gtk.main_quit);
+stage.signal.hide.connect(Clutter.main_quit);
 
 stage.color = {alpha: 0};
 stage.set_size((tiles_w * tile_size),(tiles_h * tile_size));
 
 var board = new Board();
 stage.add_actor(board);
-
-gb.get_object("new_game_item").signal.activate.connect(board.new_game);
-gb.get_object("quit_item").signal.activate.connect(Gtk.main_quit);
-
-clutter.set_size_request(stage.width, stage.height);
-window.show_all();
+stage.show_all();
 
 board.new_game();
 
-Gtk.main();
+Clutter.main();
 
