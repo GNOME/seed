7a8592c6f74d40e92b38ba9d6f0c18822bfea1d7
Tim Horton <hortont@svn.gnome.org>
2009-05-12 03:15:42 -0400

    Browser: It makes more sense to determine preferences in the preferences file.

--- examples/browser/BrowserSettings.js
+++ examples/browser/BrowserSettings.js
@@ -1,2 +1,11 @@
 var home_page = "http://www.google.com";
 var select_new_tabs = false;
+
+// Test for Gtk >= 2.16 (otherwise don't have a progress bar)
+
+var have_progress_bar = false;
+
+if(imports.gi.Gtk.Entry.prototype.set_progress_fraction)
+{
+	have_progress_bar = true;
+}
--- examples/browser/main.js
+++ examples/browser/main.js
@@ -8,15 +8,6 @@ Gtk.init(Seed.argv);
 TabbedBrowser = imports.TabbedBrowser;
 BrowserSettings = imports.BrowserSettings;
 
-// Test for Gtk >= 2.16 (otherwise don't have a progress bar)
-
-BrowserSettings.have_progress_bar = false;
-
-if(Gtk.Entry.prototype.set_progress_fraction)
-{
-	BrowserSettings.have_progress_bar = true;
-}
-
 window = new Gtk.Window({title: "Browser"});
 window.resize(800, 600);
 window.signal.hide.connect(Gtk.main_quit);
