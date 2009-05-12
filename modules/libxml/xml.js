8185b18125b663a67ec7b345c5c7de2de27144d1
Robert Carr <racarr@svn.gnome.org>
2009-05-12 15:12:15 -0400

    xml: Implement node.getElementsByTagName

--- modules/libxml/xml.js
+++ modules/libxml/xml.js
@@ -1,1 +1,12 @@
-Seed.print("XML!");
+xml = imports.libxml;
+
+xml._nodeProto.getElementsByTagName = function(name){
+    ret = [];
+    child = this.children;
+    while (child){
+	if (child.name == name)
+	    ret.push (child);
+	child = child.next;
+    }
+    return ret;
+}
