f470468c9a9bdbaef75eed578b7c0143edfb4dc9
Tim Horton <hortont@svn.gnome.org>
2009-05-13 09:39:51 -0400

    ClutterPad: Scrolled windows

--- examples/clutter-pad/main.js
+++ examples/clutter-pad/main.js
@@ -15,6 +15,9 @@ Gio = imports.gi.Gio;
 Gtk.init(Seed.argv);
 GtkClutter.init(Seed.argv);
 
+var current_filename = "";
+var stage_manager = Clutter.StageManager.get_default();
+
 var gb = new Gtk.Builder();
 gb.add_from_file("clutter-pad.ui");
 
@@ -22,17 +25,14 @@ var window = gb.get_object("window");
 var clutter = gb.get_object("clutter");
 var stage = clutter.get_stage();
 
-stage.color = {red:255, alpha: 255};
+
 
 window.resize(800, 600);
 window.show_all();
 
 Gtk.main();
 
-/*var current_filename = "";
-var stage, gtkstage = null;
-var stage_manager = Clutter.StageManager.get_default();
-
+/*
 function evaluate(button)
 {
 	var children = stage.get_children();
