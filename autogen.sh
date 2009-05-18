4e97202b9c9d0955fcfb51e799671d2cde798e81
Tim Horton <hortont@svn.gnome.org>
2009-05-17 21:46:29 -0400

    Build: Use gnome-autogen.

--- autogen.sh
+++ autogen.sh
@@ -5,41 +5,19 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="seed"
-REQUIRED_AUTOCONF_VERSION=2.53
-REQUIRED_AUTOMAKE_VERSION=1.9
-REQUIRED_GTK_DOC_VERSION=1.10
 
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/autogen.sh) || {
+(test -f $srcdir/src/main.c) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-DIE=0
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from GNOME SVN and make"
+    echo "sure the gnome-autogen.sh script is in your \$PATH."
+    exit 1
+}
 
-# This is a bit complicated here since we can't use gnome-config yet.
-# It'll be easier after switching to pkg-config since we can then
-# use pkg-config to find the gnome-autogen.sh script.
-
-gnome_autogen=
-gnome_datadir=
-
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  test -z "$dir" && dir=.
-  if test -f $dir/gnome-autogen.sh ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
-IFS="$ifs_save"
-
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
-  exit 1
-fi
-
-GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . $gnome_autogen
+REQUIRED_AUTOMAKE_VERSION=1.9 
+REQUIRED_INTLTOOL_VERSION=0.40.4
+. gnome-autogen.sh
