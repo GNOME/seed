1a599fdb7e834e5b35a2e9fbb379c9976022086e
Robert Carr <racarr@svn.gnome.org>
2009-05-25 16:42:00 -0400

    tests: Add test of the self parameter to init, and of passing additional properties to GObject constructors

---
+++ tests/javascript/gtypes/gtype-extraprop.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env seed
+// Returns: 0
+// STDIN:
+// STDOUT:1
+// STDERR:
+
+Gtk = imports.gi.Gtk;
+Gtk.init(Seed.argv);
+
+HelloWindowType = {
+    parent: Gtk.Window.type,
+    name: "HelloWindow",
+    init: function(self){
+	Seed.print(self.test);
+    }};
+
+HelloWindow = new GType(HelloWindowType);
+w = new HelloWindow({test: 1});
+
+w.show();
+      
---
+++ tests/javascript/gtypes/gtype-self.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env seed
+// Returns: 0
+// STDIN:
+// STDOUT:1
+// STDERR:
+
+Gtk = imports.gi.Gtk;
+Gtk.init(Seed.argv);
+
+HelloWindowType = {
+    parent: Gtk.Window.type,
+    name: "HelloWindow",
+    init: function(self){
+	Seed.print(this==self);
+    }};
+
+HelloWindow = new GType(HelloWindowType);
+w = new HelloWindow();
+
+w.show();
+      
