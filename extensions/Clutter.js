11bed8dabf1dea3ca2c21008128e73fbadfd2551
Robert Carr <racarr@svn.gnome.org>
2009-05-27 09:44:24 -0400

    extensions: Whoops, put Seed.printf back

--- extensions/Clutter.js
+++ extensions/Clutter.js
@@ -1,20 +1,22 @@
 Clutter = imports.gi.Clutter;
 
+var animations = new Array();
+
 Clutter.Actor.prototype.animate = function(mode, duration, json)
 {
     var properties = new Array();
     var endvalues = new Array();
-    for (var prop in json)	{
+    for (var prop in json){
 	properties.push(prop);
 	endvalues.push(new Array(this.__property_type(prop), json[prop]));
     }
     
     var animation =  this.animatev(mode, duration, properties.length, 
 				   properties, endvalues);
-    this.__animation__ = animation;
+    animations.push(animation);
     animation.timeline.signal["completed"].connect(
 	function(timeline, obj){
-	    delete obj.__animation__;
+	    animations.splice(animations.indexOf(animation),1);
 	}, this);
     
     return animation;
@@ -33,10 +35,10 @@ Clutter.Actor.prototype.animate_with_timeline = function(mode, timeline, json)
 	this.animate_with_timelinev(mode, timeline, properties.length, 
 				    properties, endvalues);
     
-    this.__animation__ = animation;
+    animations.push(animation);
     animation.timeline.signal["completed"].connect(
 	function(timeline, obj){
-	    delete obj.__animation__;
+	    animations.splice(animations.indexOf(animation),1);
 	}, this);
     
     return animation;
