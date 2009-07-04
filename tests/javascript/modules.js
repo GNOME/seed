7812a6eededd99df9e24e1937178be563a56e71d
Tim Horton <hortont424@gmail.com>
2009-07-03 22:14:37 -0400

    Add module test to check for undefined symbols and assist the coverage tool

---
+++ tests/javascript/modules.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env seed
+// Returns: 0
+// STDIN:
+// STDOUT:
+// STDERR:
+
+// This test is merely to appease the coverage checker (calls init in each
+// module); it will also catch unresolved symbol errors in the modules.
+
+try
+{
+	cairo = imports.cairo;
+	canvas = imports.canvas;
+	dbus = imports.dbus;
+	example = imports.example;
+	gettext = imports.gettext;
+	gtkbuilder = imports.gtkbuilder;
+	libxml = imports.libxml;
+	mpfr = imports.mpfr;
+	Multiprocessing = imports.Multiprocessing;
+	os = imports.os;
+	readline = imports.readline;
+	sandbox = imports.sandbox;
+	sqlite = imports.sqlite;
+}
+catch(e)
+{
+
+}
