87b930b47e17e8da289425083706058170288919
Tim Horton <hortont424@gmail.com>
2009-07-08 21:59:00 -0400

    [docs] Fix incorrect argument name in prepare_global_context docs.

--- libseed/seed-engine.c
+++ libseed/seed-engine.c
@@ -62,7 +62,7 @@ static const GDebugKey seed_debug_keys[] = {
 
 /**
  * seed_prepare_global_context:
- * @group: A #SeedContext on which to add the default set of global objects.
+ * @ctx: A #SeedContext on which to add the default set of global objects.
  *
  * Adds the default set of global objects (imports, GType, Seed, and print)
  * to a fresh #SeedContext.
