32997f9ee3d2f407125b4d6f1ee4015a750e71f4
Matt Arsenault <arsenm2@rpi.edu>
2010-01-06 14:03:33 -0500

    Change copyright header of mpfr module
    I wrote this useless part, not Robb, and it's 2010.

--- modules/mpfr/seed-mpfr-cmp.c
+++ modules/mpfr/seed-mpfr-cmp.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Seed.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) Robert Carr 2009 <carrr@rpi.edu>
+ * Copyright (C) Matthew Arsenault 2009-2010 <arsenm2@rpi.edu>
  */
 
 #include <mpfr.h>
