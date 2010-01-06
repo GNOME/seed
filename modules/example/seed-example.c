585cca1d52aa7e082aaa029c585fef8ea6d25d43
Tim Horton <hortont424@gmail.com>
2010-01-06 02:04:54 -0500

    Remove extraneous space in output

--- modules/example/seed-example.c
+++ modules/example/seed-example.c
@@ -23,6 +23,6 @@
 SeedObject
 seed_module_init(SeedEngine * eng)
 {
-  g_print("Hello Seed Module World \n");
+  g_print("Hello Seed Module World\n");
   return seed_make_object (eng->context, NULL, NULL);
 }
