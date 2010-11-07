4f78441320ca04c11183273b009bc2a1804222b5
Tim Horton <hortont424@gmail.com>
2010-11-06 22:22:22 -0400

    Fix documentation typo

--- tests/javascript/structs/testsuite.js
+++ tests/javascript/structs/testsuite.js
@@ -1,1 +1,32 @@
-../testsuite.js
+assertCount = 0
+
+function assert(a, err)
+{
+    assertCount++
+    
+    if(!a)
+    {
+        if(err)
+            print("Assertion " + assertCount + " failed: " + err + ".")
+        else
+            print("Assertion " + assertCount + " failed.")
+        
+        Seed.quit(assertCount)
+    }
+}
+
+function unreachable(err)
+{
+    if(err)
+        print("Unreachable code reached: " + err.toString() + ".")
+    else
+        print("Unreachable code reached.")
+    
+    Seed.quit(1)
+}
+
+function checkAsserts(n)
+{
+    if(assertCount != n)
+        assert(0, "Some assertions failed to run")
+}
