58415e71d8e767570428354c06dbbf9affedd5fa
Alexandre Mazari <scaroo@gmail.com>
2011-03-05 11:46:43 +0100

    Switch to LGPL2+ (See bug #643938 for details of approval by all major contributors)
    Import the LGPL2+ licence in the COPYING file
    Modify the source files headers to permit redistribution under
    LGPL2+ distribution.

--- modules/DynamicObject/seed-DynamicObject.c
+++ modules/DynamicObject/seed-DynamicObject.c
@@ -3,7 +3,7 @@ DynamicObject Seed module
 
 this free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation, either version 3 of
+published by the Free Software Foundation, either version 2 of
 the License, or (at your option) any later version.
  
 Copyright (C) 2010 Jonatan Liljedahl <lijon@kymatica.com>
