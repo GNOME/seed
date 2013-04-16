f73efe952e589e3be1508722eb1ec08c064a6fd1
Alan Knowles <alan@akbkhome.com>
2013-04-17 00:26:24 +0800

    down to 1 failure on make check - this patch fixes the argv failure - when run under make check

--- tests/javascript/argv.js
+++ tests/javascript/argv.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env seed
 
-imports.testsuite.assert(Seed.argv[0] == "seed")
+// when running make check, argv is actually path/to/lt-seed
+
+imports.testsuite.assert(Seed.argv[0] == "seed" || Seed.argv[0].match(/\/lt-seed$/))
