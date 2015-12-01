e75068bf9124528fef2f7261bee61cafe089016b
lemonsqueeze <lemonsqueeze@gmx.com>
2015-12-01 15:09:32 +0100

    repl: get all properties fix

--- extensions/repl.js
+++ extensions/repl.js
@@ -26,27 +26,45 @@ bind_cr = function(){
     lastLastLength = buffer.length;
 }
 
+function get_props_v1(obj)	// enumerable props only, not good for methods (eg String)
+{
+    var a = [];
+    for (var i in obj)
+	a.push(i);
+    return a;
+}
+
+function get_props_v2(obj)	// both enum and non enum, but misses inherited stuff (eg GtkWindow.show)
+{   return Object.getOwnPropertyNames(obj);   }
+
+function get_props(obj)		// Get all object properties
+{
+    var unique = function(value, index, self)
+    {  return self.indexOf(value) === index;   };
+    
+    var a = get_props_v1(obj).concat(get_props_v2(obj));
+    return a.filter(unique);
+}
+
 function complete_globals(text)
 {
     var glob = context.eval("(function(){ return this; })();");  // global object
-    var properties = Object.getOwnPropertyNames(glob);
-    completions = properties;
+    completions = get_props(glob);
     completions = completions.filter(function(s) { return s.indexOf(text) == 0; });
 }
 
 function complete_class_field(klass, klass_part, field_part)
 {
-    var properties = Object.getOwnPropertyNames(klass);
-    completions = properties;
+    completions = get_props(klass);
     completions = completions.filter(function(s) { return s.indexOf(field_part) == 0; });
     completions = completions.map(function(s){ return klass_part + '.' + s });
 }
 
 function complete_object_field(obj, obj_part, field_part)
 {
-    var properties = Object.getOwnPropertyNames(obj);
-//    var methods = Object.getOwnPropertyNames(obj.constructor.prototype);
-    var methods = Object.getOwnPropertyNames(Object.getPrototypeOf(obj));  // better (eg imports.os)
+    var properties = get_props(obj);
+//    var methods = get_props(obj.constructor.prototype);
+    var methods = get_props(Object.getPrototypeOf(obj));  // better (eg imports.os)
     completions = properties.concat(methods);
     completions = completions.filter(function(s) { return s.indexOf(field_part) == 0; });
     completions = completions.map(function(s){ return obj_part + '.' + s });    
