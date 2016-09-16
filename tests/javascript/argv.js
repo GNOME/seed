27595721664aa5f44d9bf3bb84abc586d6d40792
Danilo Cesar Lemes de Paula <danilo.cesar@collabora.co.uk>
2016-09-16 09:43:21 -0300

    Fixing the argv.js tests for some platforms.
    Looks like that, depending on the buildsystem, the argv value
    might be FOO/lt-seed or FOO/seed. So we are fixing this test accordingly.

--- tests/javascript/argv.js
+++ tests/javascript/argv.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env seed
 
-// when running make check, argv is actually path/to/lt-seed
+// when running make check, argv is actually path/to/lt-seed or path/to/seed
 
-imports.testsuite.assert(Seed.argv[0] == "seed" || Seed.argv[0].match(/\/lt-seed$/))
+imports.testsuite.assert(Seed.argv[0] == "seed" || Seed.argv[0].match(/\/lt-seed$/) || Seed.argv[0].match(/\/seed$/))
